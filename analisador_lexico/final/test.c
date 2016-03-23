38a2b724231b180e16e71192bf9f2b7a057e7afc
felipe <bolzin@gmail.com>
2016-03-23 16:51:37 -0300

    organizando

---
+++ analisador_lexico/final/test.c
@@ -0,0 +1,64 @@
+     int var1;
+   int var2;
+int var3;
+
+const const1 = 3;
+const  const2 = 2;
+const   const3 = 1;
+
+int     function1()   {
+   float var1;
+   /* do stuff here */
+}
+
+int    function2() {
+   float vara;
+   float varb;
+   /* do stuff here */
+}
+
+int function3() {/* do stuff here */}
+
+int
+function4()/*weird placed comment*/{/* do stuff here */
+}
+
+/*
+asdfasdf
+asdfasdfasdfa
+as
+
+
+
+
+asdf
+
+*/
+
+
+main            {/*asdfasdf
+asdfasdf
+
+*/
+   int a;
+   int b;
+   float c;
+
+   a= b + c;
+   a =b+c;
+   a= b +c;
+
+   if (a ==
+   b) {
+      b = c;
+   }
+
+   while (a < 3) {a++;}
+
+   for (i = 0;
+   i < 3;
+   i++) {
+
+   b = b;
+   }
+}
