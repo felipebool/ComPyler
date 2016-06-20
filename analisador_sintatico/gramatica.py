d4ffad0a9fc23277c464a1dc1eec3f92c13471c4
felipe <bolzin@gmail.com>
2016-06-20 20:50:20 -0300

    limpando

--- analisador_sintatico/gramatica.py
+++ analisador_sintatico/gramatica.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#"THE BEER-WARE LICENSE" (Revision 42):
-#<raphael.ribas@gmail.com> wrote this file. As long as you retain this notice you
-#can do whatever you want with this stuff. If we meet some day, and you think this
-#stuff is worth it, you can buy me a beer in return. Raphel Henrique Ribas
-
 import copy
 import pdb
 
