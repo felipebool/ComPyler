0610c50e13885c9708e1617fd17629e504c300bf
felipe <bolzin@gmail.com>
2016-06-20 20:47:24 -0300

    sintatic

---
+++ analisador_sintatico/gramatica.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#"THE BEER-WARE LICENSE" (Revision 42):
+#<raphael.ribas@gmail.com> wrote this file. As long as you retain this notice you
+#can do whatever you want with this stuff. If we meet some day, and you think this
+#stuff is worth it, you can buy me a beer in return. Raphel Henrique Ribas
+
+import copy
+import pdb
+
+EPSILON = 'EPSILON'
+INICIO = 'S'
+FIM = '#'
+
+def iter_r(seq):
+    for i, d in enumerate(seq):
+        yield (d, seq[i+1:])
+
+class Producao(object):
+    def __init__(self, simbolo, produz):
+        self.__simbolo__ = simbolo
+        self.__produz__ = tuple(produz)
+	
+    def simbolo(self):
+        return self.__simbolo__
+
+    def produz(self):
+        return self.__produz__
+
+    def __repr__(self):
+        rhs = ''
+        for simbolo in self.__produz__:
+            rhs += ' ' + str(simbolo)
+        return ('%s ->%s' % (self.__simbolo__, rhs))
+
+    def __hash__(self):
+        return hash(self.__simbolo__) ^ hash(self.__produz__)
+
+    def __cmp__(self, prod):
+        return self.__simbolo__ == prod.__simbolo__ and self.__produz__ == prod.__produz__
+
+class Gramatica(object):
+    def __init__(self, regras, inicial):
+        self.__regras__      = (Producao(INICIO, (inicial, FIM) ),) + tuple( Producao(regra[0], tuple(regra[1])) for regra in regras )
+        self.__regras_dict__ = dict()
+
+        for regra in self.__regras__:
+            self.__regras_dict__.setdefault(regra.simbolo(), set()).add(regra.produz())
+
+        self.__nterminais__ = set( regra.simbolo() for regra in self.__regras__ )
+        self.__simbolos__   = set( simbolo for regra in self.__regras__ for simbolo in regra.produz() ) | self.__nterminais__
+        self.__terminais__  = self.__simbolos__ - self.__nterminais__
+        self.__first__      = dict( (simbolo, set()) for simbolo in self.__nterminais__)
+        self.__follow__     = dict( (simbolo, set()) for simbolo in self.__simbolos__)
+
+        def fi(simbolos):
+            if len(simbolos) == 0:
+                return set([EPSILON])
+            resp = set()
+            for simbolo in simbolos:
+                if simbolo in self.__terminais__:
+                    resp.add(simbolo)
+                    break
+                elif not (EPSILON in self.__first__[simbolo]):
+                    resp |= (self.__first__[simbolo])
+                    break
+                else:
+                    resp |= (self.__first__[simbolo] - set([EPSILON]))
+            else:
+                resp.add(EPSILON)
+            return resp
+
+        change = True
+        while change:
+            change = False
+            for regra in self.__regras__:
+                f = fi(regra.produz())
+                if not self.__first__[regra.simbolo()] >= f:
+                    change = True
+                    self.__first__[regra.simbolo()] |= f
+
+        change = True
+        while change:
+            change = False
+            for regra in self.__regras__:
+                for simbolo, resto in iter_r(regra.produz()):
+                    f = fi(resto)
+                    if not self.__follow__[simbolo] >= f - set([EPSILON]):
+                        change = True
+                        self.__follow__[simbolo] |= f - set([EPSILON])
+                    if EPSILON in f:
+                        if not self.__follow__[simbolo] >= self.__follow__[regra.simbolo()]:
+                            change = True
+                            self.__follow__[simbolo] |= self.__follow__[regra.simbolo()]
+
+    def sequencia(self, simbolo):
+        return self.__follow__[simbolo]
+
+    def terminais(self):
+        return self.__terminais__
+
+    def nterminais(self):
+        return self.__nterminais__
+
+    def producoes(self, simbolo):
+        return self.__regras_dict__[simbolo]
+
---
+++ analisador_sintatico/slr.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#"THE BEER-WARE LICENSE" (Revision 42):
+#<raphael.ribas@gmail.com> wrote this file. As long as you retain this notice you
+#can do whatever you want with this stuff. If we meet some day, and you think this
+#stuff is worth it, you can buy me a beer in return. Raphel Henrique Ribas
+
+import sys
+import copy
+import gramatica
+
+class Item(object):
+    def __init__(self, simbolo, produz, pos = 0):
+        self.__simbolo__ = simbolo
+        self.__produz__ = tuple(produz)
+        self.__pos__ = pos
+	
+    def proximoItem(self):
+        return Item(self.__simbolo__, self.__produz__, self.__pos__ + 1)
+
+    def proximoSimbolo(self):
+        if self.__pos__ == len(self.__produz__):
+            return gramatica.EPSILON
+        else:
+            return self.__produz__[self.__pos__]
+	
+    def fim(self):
+        return self.__pos__ == len(self.__produz__)
+	
+    def simbolo(self):
+        return self.__simbolo__
+
+    def producao(self):
+        return gramatica.Producao(self.__simbolo__, self.__produz__)
+
+    def __hash__(self):
+        return hash(self.__simbolo__) ^ hash(self.__produz__)
+
+    def __eq__(self, item):
+        return self.__simbolo__ == item.__simbolo__ and self.__produz__ == item.__produz__ and self.__pos__ == item.__pos__
+
+    def __repr__(self):
+        rhs = ''
+        for n, simbolo in enumerate(self.__produz__):
+            if self.__pos__ == n:
+                rhs += '. '
+            rhs += str(simbolo) + ' '
+        if self.__pos__ == len(self.__produz__):
+            rhs += '.'
+        return '%s -> %s' % (self.__simbolo__, rhs)
+
+class Estado(object):
+    def __init__(self, itens):
+        self.__itens__ = set(itens)
+	
+    def fecha(self, gram):
+        novos = list(self.__itens__)
+        for item in novos:
+            simbolo = item.proximoSimbolo()
+            if simbolo in gram.nterminais():
+                fecho = set( Item(simbolo, produz) for produz in gram.producoes(simbolo) )
+                fecho -= self.__itens__
+                self.__itens__ |= fecho
+                novos.extend(fecho)
+
+		
+    def filhos(self, gramatica):
+        d = dict()
+        for item in self.__itens__:
+            if not item.fim():
+                d.setdefault(item.proximoSimbolo(), set()).add(item.proximoItem())
+        for simbolo, itens in d.iteritems():
+            e =  Estado(itens)
+            e.fecha(gramatica)
+            yield simbolo, e
+	
+    def itens(self):
+        for item in self.__itens__:
+            yield item
+
+    def __hash__(self):
+        return hash(frozenset(self.__itens__))
+
+    def __eq__(self, estado):
+        return self.__itens__ == estado.__itens__
+
+    def __repr__(self):
+        return ' ' + '\n '.join(str(item) for item in self.__itens__)
+
+
+NADA = ''
+REDUZ = 'Reduz'
+EMPILHA = 'Empilha'
+ACEITA = 'ac'
+		
+
+class Slr(object):
+    def __init__(self, gramatica):
+        self.__gramatica__ = gramatica
+        self.__geraEstados__()
+	
+    def __geraEstados__(self):
+        self.__estados__ = []
+
+        estado0 = Estado( Item(gramatica.INICIO, produz) for produz in self.__gramatica__.producoes(gramatica.INICIO))
+        estado0.fecha(self.__gramatica__)
+
+        feitos = set((estado0,))
+
+        fila = list((estado0,))
+
+        tabela = list()
+
+        for numero, estado in enumerate(fila):
+            tabela.append(dict())
+            print '%d:\n' % numero, estado
+            for item in estado.itens():
+                if item.fim():
+                    for simbolo in self.__gramatica__.sequencia(item.simbolo()):
+                        if tabela[numero].setdefault(simbolo, (NADA, 0))[0] == REDUZ:
+                            raise Exception('reduz-reduz detectado!')
+                        if tabela[numero][simbolo][0] == NADA:
+                            tabela[numero][simbolo] = (REDUZ, item.producao())
+            for simbolo, filho in estado.filhos(self.__gramatica__):
+                if tabela[numero].setdefault(simbolo, (NADA, 0))[0] == REDUZ:
+                    print 'Aviso! Conflito empilha-reduz no simbolo "%s"' % (simbolo)
+                if simbolo == gramatica.FIM:
+                    tabela[numero][simbolo] = (ACEITA, 0)
+                elif not filho in feitos:
+                    tabela[numero][simbolo] = (EMPILHA, len(fila))
+                    feitos.add(filho)
+                    fila.append(filho)
+                else:
+                    tabela[numero][simbolo] = (EMPILHA, fila.index(filho))
+        print 'tabela:'
+        for estado, linha in enumerate(tabela):
+            print estado, ': ' , linha
+        self.__tabela__ = tabela
+
+	
+    def parse(self, simbolos):
+        simbolos = list(simbolos)
+
+        aux_list = list()
+        for i in simbolos:
+            aux_list.append(i.split(';', 1)[1][:-1])
+        simbolos = aux_list
+
+        print '-----------------------------------------'
+
+        simbolos.append(gramatica.FIM)
+        aceita = False
+        pilha = []
+        pilha2 = []
+        estado = 0
+        reduzi = False
+        iter_simbolos = iter(simbolos)
+        simbolo = iter_simbolos.next()
+        while not aceita:
+            if not self.__tabela__[estado].has_key(simbolo):
+                print '-------------'
+                print simbolo
+                print '-------------'
+                raise('simbolo "%s" nao existe no estado %s' % (str(simbolo), str(estado)))
+            acao = self.__tabela__[estado][simbolo]
+            if acao[0] == ACEITA:
+                aceita = True
+            elif acao[0] == EMPILHA:
+                pilha.append(estado)
+                pilha2.append(simbolo)
+                estado = acao[1]
+                simbolo = iter_simbolos.next()
+            elif acao[0] == REDUZ:
+                for i in xrange(len(acao[1].produz())):
+                    estado = pilha.pop()
+                    pilha2.pop()
+                pilha.append(estado)
+                pilha2.append(acao[1].simbolo())
+                estado = self.__tabela__[estado][acao[1].simbolo()][1]
+                print acao[1]
+            print ' '.join(pilha2)
+        return True
+
